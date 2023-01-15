e3b821e3f83310afdeacdd0d2ab30294e8199b77
Garrett <garrettjrh@live.com>
2023-01-15 07:27:40 -0700

    Started work on the movie browse tab styling and javascript.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // Data Requests http://www.omdbapi.com/?apikey=[yourkey]&
 // IMG Reqests http://img.omdbapi.com/?apikey=[yourkey]&
 // API Key 373b4567
+
+const movieHTML = document.querySelector(".movies--wrapper");
+
+async function getMovieData() {
+    const movieData = await fetch("http://www.omdbapi.com/?apikey=373b4567&s=all");
+    const movieList = await movieData.json();
+    console.log(movieList)
+
+}
+
+getMovieData();
