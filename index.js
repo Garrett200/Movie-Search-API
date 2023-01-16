5cae77dea354e70be8f5870da87dcb50b635c494
Garrett <garrettjrh@live.com>
2023-01-16 08:05:37 -0700

    Making progress on the search function.

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
 // IMG Reqests http://img.omdbapi.com/?apikey=[yourkey]&
 // API Key 373b4567
 
-const movieHTML = document.querySelector(".movies--wrapper");
+async function renderMovies() {
+    const moviesWrapper = document.querySelector(".movies--wrapper");
 
-async function getMovieData() {
-    const movieData = await fetch("http://www.omdbapi.com/?apikey=373b4567&s=all");
-    const movieList = await movieData.json();
-    console.log(movieList)
+    const moviesFetch = await fetch("http://www.omdbapi.com/?apikey=373b4567&s=Tokyo-Drift");
+    const moviesData = await moviesFetch.json();
+    console.log(moviesData.Search)
+
+    movieArr = moviesData.Search.map((movie) => movie);
+
+    console.log(movieArr[0].Title)
 
 }
 
-getMovieData();
+renderMovies();
+
+function getUserInput() {
+    let search = document.querySelector(".header__search").value;
+    console.log(search)
+}
+
+getUserInput();
