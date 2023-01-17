102c6567e07b11706c60e694ee65fa2908db41ab
Garrett <garrettjrh@live.com>
2023-01-17 08:32:20 -0700

    Added some styling for the movie and movies--wrapper classes. The movies, titles, years, and media type now display on the page.

--- index.js
+++ index.js
@@ -5,21 +5,45 @@
 async function renderMovies() {
     const moviesWrapper = document.querySelector(".movies--wrapper");
 
-    const moviesFetch = await fetch("http://www.omdbapi.com/?apikey=373b4567&s=Tokyo-Drift");
+    const moviesFetch = await fetch(`http://www.omdbapi.com/?apikey=373b4567&s=${getUserInput(event)}`);
     const moviesData = await moviesFetch.json();
-    console.log(moviesData.Search)
 
-    movieArr = moviesData.Search.map((movie) => movie);
+    console.log(moviesData);
+    movieHTML = moviesData.Search.map((movie) => {
+        return `<div class="movie">
+                    <img class="movie__img" src="${movie.Poster}" alt="">
+                    <h4 class="movie__title">${movie.Title}</h4>
+                    <p class="movie__year">${movie.Year}</p>
+                    <p class="media__type">${movie.Type}</p>
+                </div>`
+    }).join('');
 
-    console.log(movieArr[0].Title)
+    moviesWrapper.innerHTML = movieHTML;
 
 }
+
+
+
+
+function movieHTML() {
+    return `<div class="movie">
+                <img class="movie__img" src="" alt="">
+                <h4 class="movie__title"></h4>
+                <p class="movie__year"></p>
+                <p class="media__type"></p>
+            </div>`
+
+}
+movieHTML();
+
+function getUserInput(event) {
+    let input = document.querySelector(".header__search").value;
+
+    return input;
+
+}
+getUserInput(event);
+
+
 
 renderMovies();
-
-function getUserInput() {
-    let search = document.querySelector(".header__search").value;
-    console.log(search)
-}
-
-getUserInput();
