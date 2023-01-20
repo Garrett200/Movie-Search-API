1c5d45e1bcef9b565d0280897ae5c18d7cf0f274
Garrett <garrettjrh@live.com>
2023-01-20 05:08:08 -0700

    Made some styling adjustments to the browse movies page. Started work on the sort functions, Old to new, New to old finished.

--- index.js
+++ index.js
@@ -1,53 +1,58 @@
 // Data Requests http://www.omdbapi.com/?apikey=[yourkey]&
 // IMG Reqests http://img.omdbapi.com/?apikey=[yourkey]&
 // API Key 373b4567
+const moviesWrapper = document.querySelector(".movies--wrapper");
 
-async function renderMovies() {
-    const moviesWrapper = document.querySelector(".movies--wrapper");
-
+async function renderMovies(filter) {
     const moviesFetch = await fetch(`http://www.omdbapi.com/?apikey=373b4567&s=${getUserInput(event)}`);
     const moviesData = await moviesFetch.json();
 
-    console.log(moviesData);
-    movieHTML = moviesData.Search.map((movie) => {
-        return `<div class="movie">
-                    <img class="movie__img" src="${movie.Poster}" alt="">
-                    <h4 class="movie__title">${movie.Title}</h4>
-                    <p class="movie__year">${movie.Year}</p>
-                    <p class="media__type">${movie.Type}</p>
-                </div>`
-    }).join('');
+    let movies = moviesData.Search
 
-    moviesWrapper.innerHTML = movieHTML;
+    if (filter === 'OLD_TO_NEW') {
+        moviesData.Search.sort((a, b) => a.Year - b.Year)
+        console.log('Sorting successful, old to new.')
+    }
+    else if (filter === 'NEW_TO_OLD') {
+        moviesData.Search.sort((a, b) => b.Year - a.Year)
+        console.log('Sorting successful, new to old.')
+    }
+    else if (filter === 'TYPE') {
+        if (movies.Type === 'movie') {
+            return movies.filter(movie => movie.Type === 'movie')
+        }
+        else {
+            console.log('all movies')
+        }
+    }
+
+    movieInnerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join('');
+    moviesWrapper.innerHTML = movieInnerHTML;
+    sortedMovies = moviesData.Search.sort((a, b) => a.Year - b.Year)
+    console.log(sortedMovies)
 
 }
 
-
-
-
-
-function movieHTML() {
+function movieHTML(movie) {
     return `<div class="movie">
-                <img class="movie__img" src="" alt="">
-                <h4 class="movie__title"></h4>
-                <p class="movie__year"></p>
-                <p class="media__type"></p>
+                <img class="movie__img" src="${movie.Poster}" alt="">
+                <h4 class="movie__title">${movie.Title}</h4>
+                <p class="movie__year">${movie.Year}</p>
+                <p class="media__type">${movie.Type}</p>
             </div>`
 
 }
-movieHTML();
 
-function getUserInput(event) {
+function getUserInput() {
     let input = document.querySelector(".header__search").value;
-
     return input;
-
 }
-getUserInput(event);
+
+function filterChange(event) {
+    return renderMovies(event.target.value)
+}
 
 const source = document.querySelector(".header__search")
 source.addEventListener('input', renderMovies)
-
-
 
 renderMovies();
