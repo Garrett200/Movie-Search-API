1d011c8de42e2c4d854dbb2a9f75fc259c621bd5
Garrett <garrettjrh@live.com>
2023-01-24 08:37:41 -0700

    Finished all of the  filter functions as well as reformatted the 'renderMovies' function. Began work on making a loading animation for the browse page, and added a hover effect to the movies.

--- index.js
+++ index.js
@@ -8,13 +8,26 @@ async function renderMovies(filter) {
     const moviesFetch = await fetch(`http://www.omdbapi.com/?apikey=373b4567&s=${getUserInput(event)}`);
     let moviesData = await moviesFetch.json();
 
+    if (filter === 'ALPHABETICAL') {
+        moviesData.Search.sort((a, b) => {
+            if (a.Title < b.Title) {
+                return -1;
+            }
+            if (a.Title > b.Title) {
+                return 1;
+            }
+            return 0;
+        })
+        console.log('alphabetical')
+    }
+
     if (filter === 'OLD_TO_NEW') {
         moviesData.Search.sort((a, b) => {
             var yearA = a.Year.indexOf("-") == -1 ? parseInt(a.Year) : parseInt(a.Year.split("-")[0]);
             var yearB = b.Year.indexOf("-") == -1 ? parseInt(b.Year) : parseInt(b.Year.split("-")[0]);
             return yearA - yearB
         })
-        console.log('old to new.')
+        console.log('old to new')
     }
 
     if (filter === 'NEW_TO_OLD') {
@@ -23,18 +36,20 @@ async function renderMovies(filter) {
             var yearB = b.Year.indexOf("-") == -1 ? parseInt(b.Year) : parseInt(b.Year.split("-")[0]);
             return yearB - yearA
         })
-        console.log('new to old.')
+        console.log('new to old')
     }
-    console.log(moviesData.Search)
+
     let movieInnerHTML = moviesData.Search.map((movie) => movieHTML(movie)).slice(0, 9).join('');
 
     if (filter === 'TYPE_MOVIE') {
         let sortToMovie = moviesData.Search.filter(movie => movie.Type === 'movie')
         movieInnerHTML = sortToMovie.map((movie) => movieHTML(movie)).slice(0, 9).join('')
+        console.log('only movies')
     }
     if (filter === 'TYPE_SERIES') {
         let sortToMovie = moviesData.Search.filter(movie => movie.Type === 'series')
         movieInnerHTML = sortToMovie.map((movie) => movieHTML(movie)).slice(0, 9).join('')
+        console.log('only series')
     }
 
     moviesWrapper.innerHTML = movieInnerHTML;
@@ -55,12 +70,12 @@ function getUserInput() {
     return input;
 }
 
+const source = document.querySelector(".header__search")
+source.addEventListener('input', renderMovies)
+
 function filterChange(event) {
     return renderMovies(event.target.value)
 }
-
-const source = document.querySelector(".header__search")
-source.addEventListener('input', renderMovies)
 
 renderMovies().then(response => {
     console.log(response);    
