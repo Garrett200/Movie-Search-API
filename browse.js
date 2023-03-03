6c16274d37f9bf4d41de1d4523b5d84b8437d4e2
Garrett <garrettjrh@live.com>
2023-03-02 22:32:33 -0700

    Added a javascript function for the contact button that can be used on both pages. Added a few assets and made a menu for the contact button.

--- browse.js
+++ browse.js
@@ -15,6 +15,7 @@ let timer;
 let searchTerm;
 let movieInfo;
 let open;
+let isModalOpen = false;
 
 
 // INDEX PAGE FUNCTIONS
@@ -36,6 +37,37 @@ function closeMenu() {
         menu.style.display = 'none';
     }, 280);
 
+}
+
+function contact(event) {
+    event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'service_lo59kw2',
+            'template_sgafzrp',
+            event.target,
+            'hDCgqlu9cChmWH-zT'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at garrettjrh@live.com"
+        )
+    })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
 
 // INDEX PAGE EVENT LISTENERS
